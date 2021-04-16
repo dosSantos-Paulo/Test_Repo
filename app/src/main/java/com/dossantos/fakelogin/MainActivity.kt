362c4edbb7a9fb67f43c24e86b73d30965f12010
dosSantos-Paulo <paulo.dossantos@hotmail.com>
2021-04-15 21:58:50 -0300

    criando logica

--- app/src/main/java/com/dossantos/fakelogin/MainActivity.kt
+++ app/src/main/java/com/dossantos/fakelogin/MainActivity.kt
@@ -2,10 +2,16 @@ package com.dossantos.fakelogin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val init = 1
+        if (init == 1) {
+            Toast.makeText(this, "True", Toast.LENGTH_LONG).show()
+        }
     }
 }
